fb2767bbc7f9dc1a197cc70c33f466935fa3a79a
ME-LocalBody <localbody@yandex.ru>
2022-07-29 14:55:46 +0300

    2022-07-29

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+
+import auth from '@/store/modules/auth'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {auth},
 })
---
+++ src/store/modules/auth.js
@@ -0,0 +1,53 @@
+import authApi from '@/api/auth'
+import {setItem} from '@/helpers/persistentStorage'
+
+const state = {
+  isSubmitting: false,
+  currentUser: null,
+  validationErrors: null,
+  isloggedIn: null,
+}
+
+const mutations = {
+  registerStart(state) {
+    state.isSubmitting = true
+    state.validationErrors = null
+  },
+  registerSuccess(state, payload) {
+    state.isSubmitting = false
+    state.currentUser = payload
+    state.isloggedIn = true
+  },
+  registerFailure(state, payload) {
+    state.isSubmitting = false
+    state.validationErrors = payload
+  },
+}
+
+const actions = {
+  register(context, credentials) {
+    return new Promise((resolve) => {
+      context.commit('registerStart')
+      authApi
+        .register(credentials)
+        .then((response) => {
+          context.commit('registerSuccess', response.data.user)
+          setItem('accessToken', response.data.user.token)
+          resolve(response.data.user)
+        })
+        .catch((result) => {
+          context.commit('registerFailure', result.response.data.errors)
+        })
+    })
+
+    // setTimeout(() => {
+    //   context.commit('registerStart')
+    // }, 1000)
+  },
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+}
